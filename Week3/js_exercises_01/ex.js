43efff5ee559f14b92b0ffbf29f8937fcaa84941
AnastasiiaPimkin <pimkinaanastasiia@gmail.com>
2024-03-13 16:23:40 +0200

    create week3/ex

---
+++ Week3/js_exercises_01/ex.js
@@ -0,0 +1,29 @@
+/*JavaScript – more on data types
+The following exercise contains the following subjects:
+● data types
+Instructions
+1. Create a variable whose value holds a string.
+2. Create a variable whose value holds a number.
+3. Create a variable whose value holds an object.
+4. Create a variable whose value holds a boolean.
+5. Create a variable whose value holds null.
+6. Create a variable whose value is undefined.
+7. Create a variable whose value holds a symbol.
+Console log the types of your variables.*/
+
+
+const strVar = "string string";
+const numVar = 12345;
+const objectVar = { name: "Name", age: 30 };
+const booleanVar = true;
+const nullVar = null;
+let undefinedVar;
+const symbolVar = Symbol("symbolSymbol");
+
+console.log("String:", strVar);
+console.log("Number:", numVar);
+console.log("Object:", objectVar);
+console.log("Boolean:", booleanVar);
+console.log("Null:", nullVar);
+console.log("Undefined:", undefinedVar);
+console.log("Symbol:", symbolVar);
